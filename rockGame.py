d068d2ca977505dff511381bcaf9a6d275b54518
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-28 23:08:43 -0300

    ask question

--- rockGame.py
+++ rockGame.py
@@ -4,4 +4,9 @@
     Date: 28 Sep 2023.
     name: the Rock Paper Scissors game.
 """
-print("Welcome to the Rock Paper Scissors game")
+
+# Adding the welcome sentence
+print("Welcome to the Rock Paper Scissors game");
+
+# Ask question and give user optios
+print("Choose Rock, Paper or Scissors :")

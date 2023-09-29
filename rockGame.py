b4619b9fce9c5f8587c1d6ed00af506c77044b7b
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-09-28 23:12:31 -0300

    ASk the user

--- rockGame.py
+++ rockGame.py
@@ -9,4 +9,7 @@
 print("Welcome to the Rock Paper Scissors game");
 
 # Ask question and give user optios
-print("Choose Rock, Paper or Scissors :")
+#print("Choose Rock, Paper or Scissors :")
+
+# get the user user's choice
+choice = input("Choose Rock, Paper or Scissors :")

0a1107281e0bdeb2232c6124391d6e48c9e6e248
W0475240 <113191891+W0475240@users.noreply.github.com>
2023-10-03 23:02:04 -0300

    add the error massage

--- rockGame.py
+++ rockGame.py
@@ -4,7 +4,7 @@
     Date: 28 Sep 2023.
     name: the Rock Paper Scissors game.
 """
-
+import random
 # Adding the welcome sentence
 print("Welcome to the Rock Paper Scissors game");
 
@@ -15,7 +15,18 @@ print("Welcome to the Rock Paper Scissors game");
 options = ["Rock", "Paper", "Scissors" ]
 
 # get the user user's choice
-choice = input("Choose Rock, Paper or Scissors :").lower()
+def get_user_choice():
+    while True:
+        userChoice = input("Choose Rock, Paper or Scissors :").lower()
+        if userChoice in ["Rock, Paper or Scissors"]:
+            return userChoice
+        else:
+            print("Your choice is invalid!! Please choose Rock, Paper or Scissors. ")
+            
+computerChoice =["Rock", "Paper", "Scissors"]
 
+        if userChoice == "Rock":
+                random_answer = random.choice(["Rock", "Paper", "Scissors"])
+                answer = f" my answer is {random_answer}."
 
 
